64b6ae09a5ffa1c8c1c4b0e24e05fe871b49309a
artemkuc44 <artjoms.kucajevs@ucdconnect.ie>
2024-02-12 14:22:27 +0000

    leet

--- src/project20280/exercises/leet.java
+++ src/project20280/exercises/leet.java
@@ -1,5 +1,6 @@
 package project20280.exercises;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class leet {
@@ -20,8 +21,43 @@ public class leet {
         return count;
     }
 
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int x = 0;
+        int y = 0;
+        int j;
+        int tempIndex;
+        int tempNum;
+        while(x < nums1.length && y < n){
+
+            if(nums2[y] < nums1[x] || x > y + m -1){
+                for(j = x;j<nums1.length-1;j++){
+                    tempIndex = j+1;
+                    tempNum = nums1[j+1];
+                    nums1[tempIndex] = nums1[j++];
+                    if(j+1 != nums1.length){
+                        nums1[j+1] = tempNum;
+                    }
+                }
+
+                nums1[x] = nums2[y];
+                x++;
+                y++;
+            }else{
+                x++;
+            }
+        }
+        System.out.println(Arrays.toString(nums1));
+    }
+
     public static void main(String[] args) {
         int[] nums = {1,1,2};
         System.out.println(removeDuplicates(nums));
+
+        ////////////////
+        int[] nums1 = {5,6,7,0,0,0};
+        int[] nums2 = {1,2,3};
+
+
+        merge(nums1,3,nums2,3);
     }
 }
